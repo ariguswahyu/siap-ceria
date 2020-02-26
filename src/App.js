5addc316467d5fb96c647677891aada29d71dd8f
Arigus Wahyu <ariguswahyu.np@gmail.com>
2020-02-26 22:48:17 +0700

    memaksa user apabila belum login harus login dulu

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { useEffect } from 'react';
 import Login from './pages/login';
-
+import { Switch, Route, withRouter } from "react-router-dom";
 import SplashScreen from './pages/splashscreen';
 import Home from './pages/home';
 import './App.css';
 
-function App() {
+function App(props) {
 
   const [isLoading, setIsLoading] = React.useState(true);
 
   useEffect(() => {
+
+
+    if (localStorage.getItem("login")) {
+      console.log("login");
+    } else {
+      console.log("belum login");
+      props.history.push("/login");
+    }
+
     setTimeout(() => {
       setIsLoading(false);
     }, 5000);
@@ -19,10 +28,24 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <Home />
+    // <div className="App">
+    //   <Home />
+    // </div>
+    <div>
+      <Switch>
+        <Route path="/" exact={true}>
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/">
+          <h1>Error 404 | Page Not Found</h1>
+        </Route>
+      </Switch>
     </div>
   );
 }
 
-export default App;
+// export default App;
+export default withRouter(App);
